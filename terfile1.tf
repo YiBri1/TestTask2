85a7e4c856175814c2e5e3efd909cc6ba65d0731
YiBri1 <92730567+YiBri1@users.noreply.github.com>
2022-01-11 19:15:45 +0200

    Create terfile1.tf

---
+++ terfile1.tf
@@ -0,0 +1,75 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+resource "aws_instance" "test_webserver" {
+  ami = "ami-0ed9277fb7eb570c9"
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.hehe.id]
+  user_data = file("my_data.sh")
+}
+
+resource "aws_security_group" "hehe" {
+  name        = "hehe"
+
+  dynamic "ingress" {
+
+    for_each = ["80", "443", "8080"]
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+
+}
+
+
+
+resource "aws_eip" "eip" {
+  instance = aws_instance.test_webserver.id
+  vpc      = true
+}
+
+
+//////////////////////////DNS_stuff
+
+
+
+resource "aws_route53_zone" "yibri1" {
+  name = "yibri1.xyz"
+}
+
+resource "aws_route53_record" "www" {
+  zone_id = aws_route53_zone.yibri1.zone_id
+  name    = "www.yibri1.xyz"
+  type    = "A"
+  ttl     = "300"
+  records = [aws_eip.eip.public_ip]
+}
+
+
+////////////////////////outputs
+
+output "public_ip" {
+  value = aws_instance.test_webserver.public_ip
+}
+
+
+output "name_server"{
+  value=aws_route53_zone.yibri1.name_servers
+}
